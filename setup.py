7be711436026305ea23bc5fe0b7864c4f69bab97
Daniel Millqvist Hassini <danielhassini1@gmail.com>
2023-01-09 22:54:32 +0100

    Fixat comboboxes återlämning

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ base=base, icon="delivery.ico"
 )
 
 setup(name=product_name,
-version="1.0.15",
+version="1.0.16",
 description="WorkOrder_BETA_reportarrival",
 executables=[exe],
 options={"bdist_msi": bdist_msi_options,
